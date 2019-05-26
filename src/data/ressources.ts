93dfaf2989772d54d62a7d2aaaa1b06c6fde1807
Sir Kane <ahmadouwalyndiaye@gmail.com>
2019-05-26 03:15:01 +0000

    Added quickPick window or provider api

--- src/data/ressources.ts
+++ src/data/ressources.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import fetch, { Response } from 'node-fetch';
 import { Track } from './models/Track';
 
 // export async function fetchCategories(): Promise<Category[]> {
@@ -13,16 +13,21 @@ import { Track } from './models/Track';
 //     return json;
 // }
 
-export async function searchTracks(name: string): Promise<Track[]> {
-    const res = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + encodeURIComponent(name), {
-        headers: { 
-            'Content-Type': 'application/json',
-            'X-RapidAPI-Key': "e04b319b2emsh34ce0fea1c09f20p10ce86jsn9658c95fd1b1",
-            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-        },
-    })
-    const json = await res.json()    
-    return json["data"]
+export async function searchTracks(provider: string, name: string): Promise<Track[]> {
+    let res = new Response();
+    switch (provider) {
+        case "Deezer":
+            res = await fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + encodeURIComponent(name), {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-RapidAPI-Key': "e04b319b2emsh34ce0fea1c09f20p10ce86jsn9658c95fd1b1",
+                    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+                }
+            });
+            break;
+    }
+    const tracks = await res.json()
+    return tracks["data"]
 }
 
 // export async function fetchCategoryImageURL(name: string): Promise<string | undefined> {
--- src/extension.ts
+++ src/extension.ts
@@ -5,32 +5,40 @@ import * as mpvAPI from "node-mpv";
 const mpv = new mpvAPI();
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.commands.registerCommand('vsmp.searchMedia', async (q) => {
-		// const mediaList = await ressources.searchTracks("souleymane faye");
-		vscode.window.showInputBox().then((q) => {
-			let qsearching = q ? q : '';
-			vscode.window.registerTreeDataProvider("vsmp.mediaList", {
-				getChildren(elem) {
-					return ressources.searchTracks(qsearching);
-				},
-				getTreeItem(element: Track) {
-					return {
-						label: element.title,
-						iconPath: vscode.Uri.parse(element.album["cover_small"]),
-						command: {
-							command: "vsmp.play",
-							title: 'play',
-							arguments: [
-								vscode.Uri.parse(element.preview)
-							]
-						}
-					};
+	vscode.commands.registerCommand('vsmp.searchMedia', async () => {
+		vscode.window.showQuickPick(["Deezer"]).then((provid: any) => {
+			let provider = provid || "";
+			vscode.window.showInputBox().then((keyword: any) => {
+				if (keyword === "") {
+					vscode.window.showInformationMessage("Please enter keyword");
+					return;
 				}
-			});
-		})
-		// vscode.window.showQuickPick(mediaList.map(media => media.title));
+				vscode.window.registerTreeDataProvider("vsmp.mediaList", {
+					async getChildren() {
+						const mediaList = await ressources.searchTracks(provider, keyword);
+						if(!mediaList || !mediaList.length) {
+							vscode.window.showInformationMessage(`No results matched "${keyword}"`);
+						}
+						return mediaList;
+					},
+					getTreeItem(element: Track) {
+						return {
+							label: element.title,
+							iconPath: vscode.Uri.parse(element.album["cover_small"]),
+							command: {
+								command: "vsmp.play",
+								title: 'play',
+								arguments: [
+									vscode.Uri.parse(element.preview)
+								]
+							}
+						};
+					}
+				});
+			})
+		});
 	});
-	vscode.commands.registerCommand('vsmp.play', async (track) => {
+	vscode.commands.registerCommand('vsmp.play', async (track: any) => {
 		mpv.load('https://cdns-preview-5.dzcdn.net' + track.path);
 	});
 }
