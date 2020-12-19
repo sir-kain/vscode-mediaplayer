19946b9eb0281c4afd403cd7496f8b7fb596fa50
Sir Kane <ahmadouwalyndiaye@gmail.com>
2020-12-19 11:56:44 +0000

    - Used node-npm v2, so it fixed load playlist commands from the view - Used "googleapis" package to fixed #31 issue (Youtube o is not a function)

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -1,33 +1,38 @@
 import fetch, { Response } from 'node-fetch';
+import { google } from "googleapis";
 import * as ENV from "dotenv";
 ENV.config();
 import { Track } from './models/Track';
-import * as YouTube from "simple-youtube-api";
-const youtube = new YouTube('AIzaSyAJ_tLRbLFzmH4sChaPRBCiH3c5a3AEZdE');
 
 export async function searchTracks(provider: string, name: string): Promise<Track[]> {
     let res = new Response();
     let tracks: Array<any> = [];
     switch (provider) {
         case "YouTube":
-            tracks = await youtube.searchVideos(name, 20).catch((err: any )=> console.log('err ==>', err));
-            console.log(tracks);
-            tracks = tracks.map((data: any) => {
-                return {
-                    title: data.title,
-                    icon: data.raw.snippet.thumbnails.default.url,
-                    url: `https://www.youtube.com/watch?v=${data.id}`,
-                    description: data.description
-                };
-            });
+            try {
+                const youtube = google.youtube({
+                    version: 'v3',
+                    auth: process.env.YOUTUBE_API_KEY
+                });
+                const response = await youtube.search.list({
+                    part: ['id', 'snippet'],
+                    type: ['video'],
+                    q: name,
+                    maxResults: 20
+                });
+                if (response.status !== 200) throw response;
 
-            console.log('tracks ==>', tracks);
+
+                tracks = normalizeYoutubeResponse(response.data.items);
+            } catch (error) {
+                console.error('error youtube ==>', error);
+            }
             break;
-        case "Podcast":            
+        case "Podcast":
             res = await fetch("https://listen-api.listennotes.com/api/v2/search?type=episode&q=" + encodeURIComponent(name), {
                 headers: {
                     'Content-Type': 'application/json',
-                    'X-ListenAPI-Key': "dca4aaf4712e4faa81315fbd3aa18bd2"
+                    'X-ListenAPI-Key': process.env.LISTENNOTE_KEY || ''
                 }
             });
             let resJson = await res.json();
@@ -42,4 +47,15 @@ export async function searchTracks(provider: string, name: string): Promise<Trac
             break;
     }
     return tracks;
+}
+
+function normalizeYoutubeResponse(data: any): Array<Track> {
+    return data.map((data: any) => {
+        return {
+            title: data.snippet.title,
+            icon: data.snippet.thumbnails.medium.url,
+            url: `https://www.youtube.com/watch?v=${data.id.videoId}`,
+            description: data.snippet.description
+        };
+    });
 }
